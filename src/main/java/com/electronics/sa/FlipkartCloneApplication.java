70cb16aee43d26fd915231cea4fb477ed95caf36
nipa08das <nipa08das@gmail.com>
2024-02-19 13:40:06 +0530

    changed

--- src/main/java/com/electronics/sa/FlipkartCloneApplication.java
+++ src/main/java/com/electronics/sa/FlipkartCloneApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdvanceLevelShoppingApplication {
+public class FlipkartCloneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdvanceLevelShoppingApplication.class, args);
+		SpringApplication.run(FlipkartCloneApplication.class, args);
 	}
 
 }
